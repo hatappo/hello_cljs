2ad443f571726824cd7a32150fe3c4965cd3b11b
hata <hatappo44@gmail.com>
2016-04-03 04:02:39 +0900

    Add script that dwonload jar.

---
+++ download.sh
@@ -0,0 +1,4 @@
+curl -LO https://github.com/clojure/clojurescript/releases/download/r1.8.40/cljs.jar
+curl -O https://clojars.org/repo/cljsjs/react/0.12.2-8/react-0.12.2-8.jar
+mv ./react-0.12.2-8.jar ./lib/
+
